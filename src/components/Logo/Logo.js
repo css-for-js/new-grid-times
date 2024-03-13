186758e5e8ce800ef526108a597e1d7fa2d71b5a
Jakub Chojnacki <60758743+Jakub-Chojnacki@users.noreply.github.com>
2024-03-13 19:48:11 +0100

    first exercise finished

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import format from 'date-fns/format';
 
+import { QUERIES } from '../../constants';
+
 const Logo = (props) => {
   return (
     <Wrapper>
@@ -22,6 +24,10 @@ const Wrapper = styled.div`
 const Link = styled.a`
   font-family: var(--font-family-logo);
   font-size: 3rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: 4rem;
+  }
 `;
 
 const TodaysDate = styled.p`
